31ee5c4a81f07fefb8d2961be807bfaaa1449590
Drew Gingerich <gingerich93@gmail.com>
2018-03-12 20:57:15 -0700

    Add aim bar size tracks jump power.

--- Assets/Play/Ball/Scripts/AimBarUI.cs
+++ Assets/Play/Ball/Scripts/AimBarUI.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class AimBarUI : MonoBehaviour {
 
 	LineRenderer lineRenderer;
+	float lineStartX = 0.15f;
+	float lineEndX = 0.6f;
 
 	void Awake() {
 		lineRenderer = GetComponent<LineRenderer>();
@@ -23,14 +26,14 @@ public class AimBarUI : MonoBehaviour {
 	}
 
 	public void UpdatePosition(Vector2 compressionVector) {
-		float scale = compressionVector.magnitude;
+		float scale = Mathf.Pow(compressionVector.magnitude, 2);
 		Vector2 normalized = compressionVector.normalized;
 		Vector3 direction3D	= new Vector3(normalized.x, normalized.y, 0);
-		lineRenderer.SetPosition(0, direction3D * 0.15f);
-		lineRenderer.SetPosition(1, direction3D * 0.6f);
+		lineRenderer.SetPosition(0, direction3D * lineStartX);
+		lineRenderer.SetPosition(1, direction3D * lineEndX * scale);
 		// lineObject.transform.localScale = new Vector3 (20, scale * 4, 1);
-		Color tempColor = lineRenderer.startColor;
-		tempColor.a = scale;
-		lineRenderer.startColor = tempColor;
+		// Color tempColor = lineRenderer.startColor;
+		// tempColor.a = scale;
+		// lineRenderer.startColor = tempColor;
 	}
 }
