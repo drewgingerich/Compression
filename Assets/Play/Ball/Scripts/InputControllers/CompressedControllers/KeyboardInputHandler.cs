b3500730df09e6916dcf27acb49ad3db7862863e
Drew Gingerich <gingerich93@gmail.com>
2018-03-13 17:45:07 -0700

    Cap input direction change speed for keyboard input, to make selecting different angles possible using just four keys.

--- Assets/Play/Ball/Scripts/InputControllers/CompressedControllers/KeyboardInputHandler.cs
+++ Assets/Play/Ball/Scripts/InputControllers/CompressedControllers/KeyboardInputHandler.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 namespace CompressedControllerInput {
 	public class KeyboardInputHandler : IInputHandler {
 
-		float maxAngularMovement = 10f;
+		float maxAngularVelocity = 50f;
 		float timeCompressed;
 		InputScheme inputScheme;
-		Vector2 lastInputVector;
+		Vector2 previousInputDirection;
 
 		public KeyboardInputHandler(InputScheme inputScheme) {
 			this.inputScheme = inputScheme;
-			lastInputVector = Vector2.zero;
+			previousInputDirection = Vector2.zero;
 		}
 
 		public Vector2 GetInputVector(Vector2 collisionNormal, float timeCompressed) {
 			Vector2 inputDirection = inputScheme.GetInputDirection();
+			Vector2 adjustedDirection = AdjustDirection(inputDirection, previousInputDirection);
+			previousInputDirection = adjustedDirection;
+
 			// float angularChange = Vector2.Angle(lastInputVector, inputDirection);
 			// float angleToMove = Mathf.Min(angularChange, maxAngularMovement);
 			// Vector2 orthogonal = Quaternion.AngleAxis(90f, Vector3.forward) * lastInputVector;
@@ -25,7 +28,20 @@ namespace CompressedControllerInput {
 			// lastInputVector = Quaternion.AngleAxis(angleToMove, Vector3.forward) * inputDirection;
 			// Debug.Log(inputDirection);
 			// return lastInputVector;
-			return inputDirection;
+			return adjustedDirection;
+		}
+
+		Vector2 AdjustDirection(Vector2 inputDirection, Vector2 previousInputDirection) {
+			if (previousInputDirection == Vector2.zero)
+				return inputDirection;
+			float angularMovement = Vector2.SignedAngle(previousInputDirection, inputDirection);
+			float maxAngularMovement = maxAngularVelocity * Time.deltaTime;
+			Debug.Log(maxAngularMovement);
+			if (Mathf.Abs(angularMovement) <= maxAngularMovement)
+				return inputDirection;
+			float clampedMovement = angularMovement >=0 ? maxAngularMovement : -maxAngularMovement;
+			Quaternion rotation = Quaternion.AngleAxis(clampedMovement, Vector3.forward);
+			return rotation * previousInputDirection;
 		}
 	}
 }
