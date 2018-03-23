783ccb8b60fb76878741cd242b687d56a9995b47
Drew Gingerich <gingerich93@gmail.com>
2018-03-22 21:19:44 -0700

    Remove reference to namespace that no longer exists.

--- Assets/Gameplay/Ball/Scripts/InputControllers/CompressedControllers/IInputHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace CompressedControllerInput {
-	public interface IInputHandler {
-		Vector2 GetInputVector(Vector2 collisionNormal);
-	}
-}
