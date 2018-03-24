faf0f66c50a89edf50c852de7e468f42bae7963a
Drew Gingerich <gingerich93@gmail.com>
2018-03-23 21:23:14 -0700

    Create camera script to center on multiple objects.

---
+++ Assets/Gameplay/VersusCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersusCamera : MonoBehaviour {
+
+	[SerializeField] float maxMovementSpeed = 1;
+	[SerializeField] Transform fallback;
+	List<GameObject> registeredBalls;
+
+	public void RegisterBall(GameObject ball) {
+		registeredBalls.Add(ball);
+	}
+
+	public void DeregisterBall(GameObject ball) {
+		registeredBalls.Remove(ball);
+	}
+
+	void Awake() {
+		registeredBalls = new List<GameObject>();
+	}
+
+	void Update() {
+		Vector3 centerPosition = FindCenterPosition();
+		transform.position = SmoothMovement(transform.position, centerPosition, maxMovementSpeed);
+	}
+
+	Vector3 FindCenterPosition() {
+		if (registeredBalls.Count == 0)
+			return new Vector3 (fallback.position.x, fallback.position.y, transform.position.z);
+		Vector3 centerPosition = Vector3.zero;
+		foreach (GameObject ball in registeredBalls) {
+			centerPosition += ball.transform.position;
+		}
+		centerPosition *= 1f / registeredBalls.Count;
+		centerPosition.z = transform.position.z;
+		return centerPosition;
+	}
+	
+	Vector3 SmoothMovement(Vector3 position, Vector3 targetPosition, float maxMovementSpeed) {
+		float maxMovement = maxMovementSpeed * Time.deltaTime;
+		Vector3 movementVector = targetPosition - position;
+		float movement = movementVector.magnitude;
+		if (movement >= maxMovement)
+			targetPosition = transform.position + movementVector * maxMovement;
+		return targetPosition;	
+	}
+}
--- Assets/VersusWaitingRoom/BallSpawner.cs
+++ Assets/VersusWaitingRoom/BallSpawner.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnBallSpawnedEvent : UnityEvent<GameObject> {}
 
 public class BallSpawner : MonoBehaviour {
+
+	public OnBallSpawnedEvent onBallSpawned;
 
 	[SerializeField] List<Transform> spawnPoints;
 	[SerializeField] Ball ballPrefab;
 	public void SpawnBall(InputScheme player) {
-		Ball newBall = Instantiate(ballPrefab, spawnPoints[0]);
+		Ball newBall = Instantiate(ballPrefab);
+		newBall.transform.position = new Vector3(spawnPoints[0].position.x, spawnPoints[0].position.y, -1);
 		newBall.inputScheme = player;
+		onBallSpawned.Invoke(newBall.collisionManager.gameObject);
 	}
 }
