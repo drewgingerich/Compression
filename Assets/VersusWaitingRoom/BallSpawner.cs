c61c8500acf106df1a433a38a043a32ec426cabe
Drew Gingerich <gingerich93@gmail.com>
2018-03-25 11:41:32 -0700

    Add entrance to the versus waiting room, that loads the versus scene when all players have entered.

--- Assets/VersusWaitingRoom/BallSpawner.cs
+++ Assets/VersusWaitingRoom/BallSpawner.cs
@@ -16,6 +16,7 @@ public class BallSpawner : MonoBehaviour {
 		Ball newBall = Instantiate(ballPrefab);
 		newBall.transform.position = new Vector3(spawnPoints[0].position.x, spawnPoints[0].position.y, -1);
 		newBall.inputScheme = player;
+		newBall.transform.parent = transform;
 		onBallSpawned.Invoke(newBall.collisionManager.gameObject);
 	}
 }
---
+++ Assets/VersusWaitingRoom/EntranceZone.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EntranceZone : MonoBehaviour {
+
+	public UnityEvent onAllPlayersReady;
+
+	List<GameObject> registeredPlayers;
+	List<GameObject> playersReady;
+
+	void Awake() {
+		registeredPlayers = new List<GameObject>();
+		playersReady = new List<GameObject>();
+	}
+
+	public void RegisterPlayer(GameObject player) {
+		registeredPlayers.Add(player);
+	}
+
+	public void DergisterPlayer(GameObject player) {
+		registeredPlayers.Remove(player);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.GetComponent<Ball>() != null) {
+			playersReady.Add(other.gameObject);
+			CheckIfAllPlayersAreReady();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.GetComponent<Ball>() != null)
+			playersReady.Remove(other.gameObject);
+	}
+
+	void CheckIfAllPlayersAreReady() {
+		if (playersReady.Count < 2)
+			return;
+		if (playersReady.Count == registeredPlayers.Count) {
+			PlayerManager.players = (from go in playersReady select go.GetComponent<Ball>().inputScheme) as List<InputScheme>;
+			onAllPlayersReady.Invoke();
+		}
+	}
+}
