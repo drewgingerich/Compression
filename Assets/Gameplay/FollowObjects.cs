6e359b04c6205593c4dc8f8a3d76efae27472382
Drew Gingerich <gingerich93@gmail.com>
2018-03-26 20:30:51 -0700

    Split VersusCamera script into two: one that follows objects and one that resizes the camera. Made a multiplayer camera prefab.

---
+++ Assets/Gameplay/FollowObjects.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObjects : MonoBehaviour {
+
+	[SerializeField] float speed = 1;
+	[SerializeField] GameObject fallbackObject;
+	[SerializeField] List<GameObject> objectsToFollow;
+
+	public void AddObject(GameObject gameObject) {
+		objectsToFollow.Add(gameObject);
+	}
+
+	public void RemoveObject(GameObject gameObject) {
+		objectsToFollow.Remove(gameObject);
+	}
+
+	void Awake() {
+		objectsToFollow = new List<GameObject>();
+	}
+	
+	void Update() {
+		Vector3 centerPosition = FindCenterPosition();
+		Vector3 smoothedPosition = SmoothMovement(transform.position, centerPosition, speed);
+		transform.position = smoothedPosition;
+	}
+
+	Vector3 FindCenterPosition() {
+		Vector3 centerPosition = Vector3.zero;
+		foreach (GameObject objectToFollow in objectsToFollow) {
+			if (objectToFollow != null)
+				centerPosition += objectToFollow.transform.position;
+		}
+		if (centerPosition == Vector3.zero) {
+			Vector3 fallbackPosition = fallbackObject.transform.position;
+			float currentDepth = transform.position.z;
+			return new Vector3(fallbackPosition.x, fallbackPosition.y, currentDepth);
+		} else {
+			centerPosition *= 1f / objectsToFollow.Count;
+			centerPosition.z = transform.position.z;
+			return centerPosition;
+		}
+	}
+
+	Vector3 SmoothMovement(Vector3 position, Vector3 targetPosition, float maxMovementSpeed) {
+		float maxMovement = maxMovementSpeed * Time.deltaTime;
+		Vector3 movementVector = targetPosition - position;
+		float movement = movementVector.magnitude;
+		if (movement > maxMovement)
+			targetPosition = transform.position + movementVector * maxMovement;
+		return targetPosition;
+	}
+}
---
+++ Assets/Gameplay/ResizeForObjects.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class ResizeForObjects : MonoBehaviour {
+
+	[SerializeField] float growSpeed = 2; 
+	[SerializeField] float shrinkSpeed = 0.5f;
+	[SerializeField] float baseOrthographicSize = 6;
+	[SerializeField] float buffer = 2.5f;
+	[SerializeField] List<GameObject> objects;
+
+	new Camera camera;
+
+	public void AddObject(GameObject gameObject) {
+		objects.Add(gameObject);
+	}
+
+	public void RemoveObject(GameObject gameObject) {
+		objects.Remove(gameObject);
+	}
+
+	void Awake() {
+		objects = new List<GameObject>();
+		camera = GetComponent<Camera>();
+	}
+
+	public void Update () {
+		float aspectRatio = (float)Screen.width / Screen.height;
+		float orthographicSize = FindOrthographicSize(aspectRatio);
+		float smoothedSize = SmoothSizeChange(camera.orthographicSize, orthographicSize, growSpeed, shrinkSpeed);
+		camera.orthographicSize = smoothedSize;	
+	}
+
+	float FindOrthographicSize(float aspectRatio) {
+		float currentX = transform.position.x;
+		float currentY = transform.position.y;
+		Vector2 cornerVector = Vector2.zero;
+		foreach (GameObject obj in objects) {
+			float verticalDistance = Mathf.Abs(currentY - obj.transform.position.y);
+			if (verticalDistance > cornerVector.y) {
+				cornerVector.y = verticalDistance;
+			}
+			float horizontalDistance = Mathf.Abs(currentX - obj.transform.position.x);
+			float scaledHorizontalDistance = horizontalDistance / aspectRatio;
+			if (scaledHorizontalDistance > cornerVector.x)
+				cornerVector.x = scaledHorizontalDistance;
+		}
+		cornerVector += new Vector2(buffer, buffer);
+		float verticalSize = Mathf.Max(cornerVector.y, baseOrthographicSize);
+		float horizontalSize = Mathf.Max(cornerVector.x, baseOrthographicSize);
+		return Mathf.Max(verticalSize, horizontalSize);
+	}
+
+	float SmoothSizeChange(float size, float targetSize, float growSpeed, float shrinkSpeed) {
+		float change = targetSize - size;
+		float maxGrowth = growSpeed * Time.deltaTime;
+		float maxShrink = -shrinkSpeed * Time.deltaTime;
+		if (change > maxGrowth) {
+			return size + maxGrowth;
+		} else if (change < maxShrink) {
+			return size + maxShrink;
+		} else {
+			return targetSize;
+		}
+	}
+}
