348d6a800ecfe784e77e2045abc99b1eeb43e18d
Drew Gingerich <gingerich93@gmail.com>
2018-03-29 22:09:32 -0700

    Add gravity well prototype.

---
+++ Assets/ArcCollider.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnTrigger2DEvent : UnityEvent<Rigidbody2D> {}
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class ArcCollider : MonoBehaviour {
+
+	public OnTrigger2DEvent OnEnterArc;
+	public OnTrigger2DEvent OnExitArc;
+	public OnTrigger2DEvent OnStayArc;
+
+	[Header("Zero is to the right ->")] 
+	[SerializeField] [Range(0, 360)] float arcCenterAngle;
+	[SerializeField] [Range(0, 180)] float arcLengthHalfAngle;
+
+	List<Collider2D> others;
+
+	void Awake() {
+		others = new List<Collider2D>();
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		float angle = FindAngle(other.transform.position);
+		if (angle <= arcLengthHalfAngle) {
+			if (!others.Contains(other))
+				Add(other);
+			else
+				OnStayArc.Invoke(other.GetComponent<Rigidbody2D>());
+		} else {
+			Remove(other);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		float angle = FindAngle(other.transform.position);
+		if (angle <= arcLengthHalfAngle)
+			Add(other);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		Remove(other);
+	}
+
+	void Add(Collider2D other) {
+		others.Add(other);
+		Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
+		OnEnterArc.Invoke(rb2d);
+	}
+
+	void Remove(Collider2D other) {
+		others.Remove(other);
+		Rigidbody2D rb2d = other.GetComponent<Rigidbody2D>();
+		OnExitArc.Invoke(rb2d);
+	}
+
+	float FindAngle(Vector3 otherPosition) {
+		Quaternion rotation = Quaternion.AngleAxis(arcCenterAngle, Vector3.forward);
+		Vector2 arcCenterVector = rotation * Vector2.right;
+		Vector2 triggerVector = otherPosition - transform.position;
+		return Vector3.Angle(arcCenterVector, triggerVector);
+	}
+}
---
+++ Assets/Test/GravityWell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWell : MonoBehaviour {
+
+	[SerializeField] float strength = 1f;
+
+	[SerializeField] List<Rigidbody2D> victims;
+
+	public void AddVictim(Rigidbody2D victim) {
+		victims.Add(victim);
+	}
+
+	public void RemoveVictim(Rigidbody2D victim) {
+		Debug.Log("Hi");
+		victims.Remove(victim);
+	}
+
+	void Awake() {
+		victims = new List<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		foreach(Rigidbody2D victim in victims) {
+			Vector3 distanceVector = transform.position - victim.transform.position;
+			float distance = distanceVector.magnitude;
+			float gravityMagnitude = strength * victim.mass * Mathf.Pow(distance, -1.75f);
+			Vector3 gravityForce = distanceVector.normalized * gravityMagnitude;
+			victim.AddForce(gravityForce);
+		}
+	}
+}
