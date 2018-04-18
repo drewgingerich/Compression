ba48fd6240e43818af0ba6da91f5a6eb635032ff
Drew Gingerich <gingerich93@gmail.com>
2018-04-17 18:06:46 -0700

    PlayerInfo can copy values from other player info instances, to make passing information accross scenes possible.

--- Assets/_game/Scripts/Ball/PlayerInfo/PlayerInfo.cs
+++ Assets/_game/Scripts/Ball/PlayerInfo/PlayerInfo.cs
@@ -8,4 +8,10 @@ public class PlayerInfo : ScriptableObject {
 	public InputSchemeEventValue inputScheme;
 	public IntEventValue id;
 	public PlayerColorSchemeEventValue colorScheme;
+	
+	public void LoadFromReference(PlayerInfo playerInfo) {
+		inputScheme.Value = playerInfo.inputScheme.Value;
+		id.Value = playerInfo.id.Value;
+		colorScheme.Value = playerInfo.colorScheme.Value;
+	}
 }
