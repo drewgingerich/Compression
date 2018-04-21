966efc421983201c33798eaae0eabe6379b820d1
Drew Gingerich <gingerich93@gmail.com>
2018-04-21 11:16:09 -0700

    Add BallAnimation script that plays animations based on the ball's state changes.

---
+++ Assets/_game/Scripts/Ball/Visuals/BallAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnimation : MonoBehaviour
+{
+	[SerializeField] Ball ball;
+	[SerializeField] Animator anim;
+
+	int squishedHash;
+
+	void Awake()
+	{
+		squishedHash = Animator.StringToHash("Squished");
+		ball.state.stateName.OnChange += AnimateBall;
+	}
+
+	void AnimateBall(StateName stateName)
+	{
+		if (stateName == StateName.Compressed || stateName == StateName.Rebound)
+		{
+			anim.SetBool(squishedHash, true);
+		}
+		else
+		{
+			anim.SetBool(squishedHash, false);
+		}
+	}
+}
