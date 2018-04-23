b57ee0cb2816f874b025380ad90d026111fc9d78
Drew Gingerich <gingerich93@gmail.com>
2018-04-22 17:05:52 -0700

    Add Clamp function as a Vector2 extension method.

---
+++ Assets/_game/Scripts/Utils/Vector2Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector2Extensions {
+
+	public static Vector2 Clamp(this Vector2 vector, Vector2 reference, float maxAngle) {
+		float angle = Vector2.SignedAngle(reference, vector);
+		if (Mathf.Abs(angle) <= maxAngle)
+			return vector;
+		float clampedAngle = Mathf.Clamp(angle, -maxAngle, maxAngle);
+		Quaternion rotation = Quaternion.AngleAxis(clampedAngle, Vector3.forward);
+		return rotation * reference;
+	}
+}
