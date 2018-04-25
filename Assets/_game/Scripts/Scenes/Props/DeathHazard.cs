d0c6fde07468b11e64eacb12d3d6edf4f3cdc29e
Drew Gingerich <gingerich93@gmail.com>
2018-04-24 18:49:20 -0700

    Create new versus level with deadly spikes.

---
+++ Assets/_game/Scripts/Scenes/Props/DeathHazard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHazard : MonoBehaviour {
+
+	[SerializeField] GameObjectRuntimeSet ballGameObjects;
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		GameObject otherObject = collision.gameObject;
+		if (ballGameObjects.items.Contains(otherObject))
+			Destroy(otherObject);
+	}
+}
