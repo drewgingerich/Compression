517693994161bd65a3408bf7b339c4e82d93e9e0
Drew Gingerich <gingerich93@gmail.com>
2018-04-26 21:10:57 -0700

    Add moving walls to SpikePit level that force players out of the starting safe zone.

---
+++ Assets/SnapTrap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapTrap : MonoBehaviour {
+
+	[SerializeField] Rigidbody2D snapObject;
+	[SerializeField] float snapTime;
+
+	Vector3 originalPosition;
+	bool armed = true;
+
+	void Start() {
+		originalPosition = snapObject.transform.position;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (armed) {
+			armed = false;
+			StartCoroutine(SnapObjectToPosition());
+		}
+	}
+
+	IEnumerator SnapObjectToPosition() {
+		yield return new WaitForFixedUpdate();
+		float progress = 0f;
+		while (true) {
+			progress += Time.fixedDeltaTime / snapTime;
+			Vector3 newPosition = Vector3.Lerp(originalPosition, transform.position, progress);
+			snapObject.MovePosition(newPosition);
+			if (progress >= 1)
+				break;
+			else
+				yield return new WaitForFixedUpdate();
+		}
+	}
+}
---
+++ Assets/_game/Scripts/Tile/TileSpriteApplicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileSpriteApplicator : MonoBehaviour {
+
+	[SerializeField] SpriteRenderer spriteRenderer;
+	[SerializeField] Tile tile;
+
+	void Start() {
+		ApplyTileSprite();
+		gameObject.AddComponent<PolygonCollider2D>();
+	}
+
+	void ApplyTileSprite() {
+		spriteRenderer.sprite = tile.sprite;
+		spriteRenderer.color = tile.color;
+	}
+}
