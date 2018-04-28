0ef5c2b7caae187b923a290407cd11eac1fad506
Drew Gingerich <gingerich93@gmail.com>
2018-04-28 14:25:26 -0700

    Create script to load Base scene if not already loaded, to move towards having every scene work as a start point.

---
+++ Assets/_game/Scripts/Scene/Infrastructure/BaseSceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaseSceneLoader : MonoBehaviour {
+
+	public event System.Action OnBaseSceneLoaded = delegate { };
+	string baseSceneName = "Base";
+
+	void Awake() {
+		Scene baseScene = SceneManager.GetSceneByName(baseSceneName);
+		if (baseScene.IsValid())
+			StartCoroutine(LoadBaseScene());
+		else
+			OnBaseSceneLoaded();
+	}
+
+	IEnumerator LoadBaseScene() {
+		yield return SceneManager.LoadSceneAsync("Base", LoadSceneMode.Additive);
+		OnBaseSceneLoaded();
+	}
+}
