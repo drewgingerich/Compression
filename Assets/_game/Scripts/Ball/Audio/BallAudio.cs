fb68f1966644d1e8b864f6c3f9c3dd42d0a445a9
Drew Gingerich <gingerich93@gmail.com>
2018-05-05 15:00:16 -0700

    Add a small pop noise when ball launches.

---
+++ Assets/_game/Scripts/Ball/Audio/BallAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAudio : MonoBehaviour {
+
+	[SerializeField] Ball ball;
+
+	[SerializeField] AudioClip launchSound;
+
+	void Awake() {
+		ball.state.previousState.OnChange += PlayBallSound;
+	}
+
+	void PlayBallSound(StateName previousStateName) {
+		if (previousStateName == StateName.Compressed || previousStateName == StateName.Rebound) {
+			AudioSource.PlayClipAtPoint(launchSound, transform.position);
+		}
+	}
+}
