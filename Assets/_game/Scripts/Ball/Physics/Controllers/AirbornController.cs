d0b2655e481e9e72646d17abdfe4438ff9cee8fc
Drew Gingerich <gingerich93@gmail.com>
2018-05-06 13:16:50 -0700

    Update min time in airborn state to physics frame (giving time to get off the ground after launch) instead of a fixed duration.

--- Assets/_game/Scripts/Ball/Physics/Controllers/AirbornController.cs
+++ Assets/_game/Scripts/Ball/Physics/Controllers/AirbornController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AirbornController : BallController {
 
+	bool delay;
+	int frameCount;
+
 	public override BallController CheckTransitions(BallState state, Rigidbody2D rb2d) {
 		if (CheckImpactTransition(state))
 			return new ImpactController();
@@ -13,13 +16,22 @@ public class AirbornController : BallController {
 	public override void Enter(BallState state, Rigidbody2D rb2d) {
 		state.stateName.Value = StateName.Airborn;
 		state.gravityRatio.Value = 1f;
+		delay = true;
+		frameCount = 0;
 	}
 
 	public override void Exit(BallState state, Rigidbody2D rb2d) {
 		state.previousState.Value = StateName.Airborn;
 	}
 
+	public override void Update(BallState state, Rigidbody2D rb2d) {
+		rb2d.AddForce(state.inputDirection.Value * 2);
+		frameCount++;
+		if (frameCount > 1)
+			delay = false;
+	}
+
 	bool CheckImpactTransition(BallState state) {
-		return state.grounded.Value;
+		return state.grounded.Value && !delay;
 	}
 }
