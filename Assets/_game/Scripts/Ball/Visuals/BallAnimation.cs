ef4aee702a25f05532e7571318b0482cdefaee08
Drew Gingerich <gingerich93@gmail.com>
2018-05-28 20:28:55 -0700

    Update ball animations to make better use of the various levels of squish in the current spritesheet.

--- Assets/_game/Scripts/Ball/Visuals/BallAnimation.cs
+++ Assets/_game/Scripts/Ball/Visuals/BallAnimation.cs
@@ -2,35 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallAnimation : MonoBehaviour
-{
+public class BallAnimation : MonoBehaviour {
+
 	[SerializeField] Ball ball;
 	[SerializeField] Animator anim;
 
 	int squishedHash;
 	int impactHash;
 
-	void Awake()
-	{
-		squishedHash = Animator.StringToHash("Squished");
-		impactHash = Animator.StringToHash("Impact");
+	int noSquishHash;
+	int lowSquishHash;
+	int medSquishHash;
+	int highSquishHash;
+
+	void Awake() {
+		noSquishHash = Animator.StringToHash("NoSquish");
+		lowSquishHash = Animator.StringToHash("LowSquish");
+		medSquishHash = Animator.StringToHash("MedSquish");
+		highSquishHash = Animator.StringToHash("HighSquish");
+
 		ball.state.stateName.OnChange += AnimateBall;
 	}
 
-	void AnimateBall(StateName stateName)
-	{
-		if (stateName == StateName.Compressed || stateName == StateName.Rebound)
-		{
-			anim.SetBool(squishedHash, true);
-		}
-		else if (stateName == StateName.Impact)
-		{
-			anim.SetBool(impactHash, true);
-		}
-		else
-		{
-			anim.SetBool(squishedHash, false);
-			anim.SetBool(impactHash, false);
+	void AnimateBall(StateName stateName) {
+		if (stateName == StateName.Compressed) {
+			anim.SetTrigger(medSquishHash);
+		} else if (stateName == StateName.Rebound) {
+			anim.SetTrigger(highSquishHash);
+		} else if (stateName == StateName.Impact) {
+			anim.SetTrigger(medSquishHash);
+		} else if (stateName == StateName.Grounded) {
+			anim.SetTrigger(lowSquishHash);
+		} else if (stateName == StateName.Airborn) {
+			anim.SetTrigger(noSquishHash);
 		}
 	}
 }
